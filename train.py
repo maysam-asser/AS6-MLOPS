336d7e48cbdf5f512603d5fc4cf229a17ba705cd
maysam-asser <your-email@gmail.com>
2026-04-15 15:29:03 +0200

    force failure test [run-train]

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 print("Training model...")
-model = RandomForestClassifier()
+model = RandomForestClassifier(max_depth=1, n_estimators=1)
 model.fit(X_train, y_train)
 
 print("Evaluating model...")
